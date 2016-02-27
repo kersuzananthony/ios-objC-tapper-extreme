8cfa103bebc4d0fb688397ec0d2facd9ac9223c2
Kersuzan <kersuzan.anthony@gmail.com>
2016-02-27 15:43:28 +0800

    Initial commit

--- tapperExtremeObjectiveC/ViewController.m
+++ tapperExtremeObjectiveC/ViewController.m
@@ -10,9 +10,18 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIImageView *tapperImage;
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
+@property (weak, nonatomic) IBOutlet UIButton *coinButton;
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+
 @end
 
 @implementation ViewController
+
+NSInteger maxTap = 0;
+NSInteger currentTap = 0;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -22,6 +31,75 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)playPressed:(UIButton *)sender {
+    
+    if ([self.textField.text isEqualToString:@""]) {
+        NSLog(@"No input found!");
+    } else {
+        maxTap = [self.textField.text integerValue];
+        
+        self.textField.hidden = YES;
+        self.playButton.hidden = YES;
+        self.tapperImage.hidden = YES;
+        
+        self.textLabel.hidden = NO;
+        self.coinButton.hidden = NO;
+        
+        [self updateTextlabel];
+    }
+    
+}
+
+- (IBAction)coinPressed:(UIButton *)sender {
+    
+    currentTap++;
+    [self updateTextlabel];
+    
+    if ([self isGameOver]) {
+        [self startGame];
+    }
+}
+
+
+
+
+
+
+
+
+
+- (void) startGame {
+
+    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL: [NSURL URLWithString:@"http://www.google.com"]]];
+    
+    currentTap = 0;
+    
+    self.coinButton.hidden = YES;
+    self.textLabel.hidden = YES;
+    
+    self.textField.hidden = NO;
+    self.playButton.hidden = NO;
+    self.tapperImage.hidden = NO;
+    
+    [self updateTextlabel];
+    
+    self.textField.text = @"";
+}
+
+- (void)updateTextlabel {
+    self.textLabel.text = [NSString stringWithFormat:@"%ld Taps", (long)currentTap];
+}
+
+-(BOOL) isGameOver {
+    
+    if (currentTap >= maxTap) {
+        return YES;
+    } else {
+        return NO;
+    }
+    
 }
 
 @end
